20f4215f78f7964cf528cacaa33209cd725900dd
moltam89 <moltam89@gmail.com>
2024-10-04 15:22:12 +0200

    Update hardhat.config.ts.template.mjs to use compilers array - https://github.com/scaffold-eth/scaffold-eth-2/pull/938

--- extension/packages/hardhat/hardhat.config.ts.args.mjs
+++ extension/packages/hardhat/hardhat.config.ts.args.mjs
@@ -1,18 +1,8 @@
-export const solidity = `solidity: {
-    compilers: [
-      {
-        version: "0.8.17",
-        settings: {
-          optimizer: {
-            enabled: true,
-            // https://docs.soliditylang.org/en/latest/using-the-compiler.html#optimizer-options
-            runs: 200,
-          },
-        },
-      },
-      {
+export const compilers = `{
         version: "0.5.16",
         settings: {},
       },
-    ],
-  },`;
+      {
+        version: "0.8.20",
+        settings: {},
+      }`;
